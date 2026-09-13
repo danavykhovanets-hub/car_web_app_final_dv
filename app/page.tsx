c36d6b6eb2277bc79338c3aaa040f8c5452d43e0
danavykhovanets-hub <dana.vykhovanets@gmail.com>
2026-09-13 16:27:39 -0400

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import css from "./page.module.css";
+
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <section className={css.hero}>
+      <div className={css.overlay}>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.subtitle}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <Link href="/catalog" className={css.button}>
+          View Catalog
+        </Link>
+      </div>
+    </section>
+  );
 }
